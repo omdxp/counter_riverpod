2003400f137ebad4aea9ef228a3276101e7a16b9
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-06-25 14:13:19 +0100

    State provider way.

--- lib/src/screens/counter_state_provider.dart
+++ lib/src/screens/counter_state_provider.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CounterStateProviderPage extends StatelessWidget {
+final _counterProvider = StateProvider((ref) => 0);
+
+class CounterStateProviderPage extends ConsumerWidget {
   const CounterStateProviderPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
+    final counter = watch(_counterProvider).state;
     return Scaffold(
       appBar: AppBar(
         title: Text('State Provider'),
+      ),
+      body: Center(child: Text('Count: $counter')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.read(_counterProvider).state++;
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
